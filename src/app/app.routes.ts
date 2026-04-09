cc3150e4fc8d7ad4e7676ced2fdc19ecdc6a89f0
Edmond Mbadu <mbadungoma@gmail.com>
2026-04-08 22:07:40 -0700

    aded login and setup page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { MarketingComponent } from './marketing/marketing';
 import { LandingComponent } from './landing/landing';
+import { SignInComponent } from './sign-in/sign-in';
+import { CreateAccountComponent } from './create-account/create-account';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password';
 
 export const routes: Routes = [
   { path: '', component: MarketingComponent },
   { path: 'landing', component: LandingComponent },
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'create-account', component: CreateAccountComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
 ];
-
