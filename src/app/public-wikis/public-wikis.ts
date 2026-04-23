703f5c198902216e90b8eec9171d12cc0a70a0a6
Edmond Mbadu <mbadungoma@gmail.com>
2026-04-22 17:12:25 -0700

    added the list of public wikis that needs to be built soon

--- src/app/public-wikis/public-wikis.ts
+++ src/app/public-wikis/public-wikis.ts
@@ -12,8 +12,10 @@ const ALL_CATEGORIES = 'All';
   templateUrl: './public-wikis.html',
 })
 export class PublicWikisComponent {
-  readonly publicWikis = PUBLIC_WIKI_CATALOG;
-  readonly featuredWikis = this.publicWikis.filter((wiki) => wiki.status === 'live').slice(0, 3);
+  readonly publicWikis = [...PUBLIC_WIKI_CATALOG].sort((a, b) => {
+    if (a.status === b.status) return 0;
+    return a.status === 'live' ? -1 : 1;
+  });
   readonly liveCount = this.publicWikis.filter((wiki) => wiki.status === 'live').length;
   readonly comingSoonCount = this.publicWikis.filter((wiki) => wiki.status === 'coming-soon').length;
 
